2c44e6bb64e2e8e67b16ac14e3da7216e5636b70
Arash Kaffamanesh <arash.kaffamanesh.ext@dkv-mobility.com>
2019-10-19 20:22:06 +0200

    region changed to eu-central-1

--- terraform-aws-vpc/examples/simple-vpc/main.tf
+++ terraform-aws-vpc/examples/simple-vpc/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-central-1"
 }
 
 data "aws_security_group" "default" {
@@ -10,11 +10,11 @@ data "aws_security_group" "default" {
 module "vpc" {
   source = "../../"
 
-  name = "simple-example"
+  name = "kubernetes-v"
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -33,7 +33,7 @@ module "vpc" {
   }
 
   vpc_tags = {
-    Name = "vpc-name"
+    Name = "kubenretes-v"
   }
 }
 
